c69c77d3ba8ce1ab521862110986687fac3a65b5
Mona <asatryanhayko92@gmail.com>
2020-07-05 20:14:18 +0400

    Bridge added

---
+++ src/bridge/CppDeveloper.java
@@ -0,0 +1,8 @@
+package bridge;
+
+public class JavaDeveloper1 implements Developer {
+    @Override
+    public void writeCode() {
+        System.out.println("Java developer wites java code...");
+    }
+}
